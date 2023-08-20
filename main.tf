a94080a7f106441e9de0f9e5f1b6a88365c88447
Armstrong Elorh <el.amy2019@gmail.com>
2023-08-20 17:48:27 -0400

    subnets creation

--- main.tf
+++ main.tf
@@ -11,3 +11,52 @@ resource "aws_vpc" "dev-vpc" {
     }
   
 }
+resource "aws_internet_gateway" "dev-gw" {
+  vpc_id = aws_vpc.dev-vpc.id
+
+  tags = {
+    Name = "dev-gw"
+  }
+}
+
+resource "aws_subnet" "dev-public-subnet1" {
+  vpc_id     = aws_vpc.dev-vpc.id
+  cidr_block = "10.0.0.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "dev-public-subnet1"
+  }
+}
+
+resource "aws_subnet" "dev-public-subnet2" {
+  vpc_id     = aws_vpc.dev-vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "dev-public-subnet2"
+  }
+}
+
+resource "aws_subnet" "dev-private-subnet1" {
+  vpc_id     = aws_vpc.dev-vpc.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "dev-private-subnet1"
+  }
+}
+
+resource "aws_subnet" "dev-public-subnet2" {
+  vpc_id     = aws_vpc.dev-vpc.id
+  cidr_block = "10.0.3.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    "Name" = "dev-public-subnet2"
+  }
+}
